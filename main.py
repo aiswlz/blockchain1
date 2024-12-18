1108486b6324bd0c55e5ca09e53f8b679d2ac493
aiswlz <230787@astanait.edu.kz>
2024-12-18 16:52:08 +0500

    hash text

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ def hash(text):
     # Hash the input text using SHA-256
     hashed = hashlib.sha256(text.encode('utf-8')).hexdigest()
     return hashed
+
+text = input("Enter your text: ")
+hashed_text = hash(text)
+print("Hashed value:", hashed_text)
